28727df2c0e03414c7d97b38761e4b9eb7d77270
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-24 18:58:38 +0300

    Create Detail Fragment

--- app/src/main/java/com/noor/weatherappuidesign/HomeFragment.kt
+++ app/src/main/java/com/noor/weatherappuidesign/HomeFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.noor.weatherappuidesign.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.tvNext.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToDetailFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
